8257dbdc790b0a51c56d04d36bce62cf6964122d
PatrickAnnese <annesepatrick02@gmail.com>
2022-12-02 10:54:29 +0100

    primo esercizio verifica

---
+++ src/BasicJava.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class BasicJava {
+    ArrayList <p> = new arrayList<>();
+    public static void printZigZag(arrayList p){
+
+
+    }
+    public static void findFirstDuplicate(){
+
+    }
+}
